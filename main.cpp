1a05458eab5aff4d9bb5989bc9a80c9ae0261612
DennisCarcamo <joel.carcamos@unitec.edu>
2016-02-26 15:02:09 -0600

    Menu casi completo

--- main.cpp
+++ main.cpp
@@ -1,9 +1,71 @@
 #include<iostream>
+#include<ncurses.h>
+#include<cstring>
+//#include "friend.h"
 
+//using std::cout;
+using std::cin;
+using std::endl;
+
+void leerimprimir(char* );
 int main(int argc, char* argv[]){
+	
+	initscr();
+	char nombre[25];
+	char numero[9];
+	numero [3]='-';
+	//Friend [];
+	move(19,35);
+	start_color();
+	init_pair(2,COLOR_GREEN,COLOR_BLACK);
+	attron(COLOR_PAIR(2));
+	printw("Bienvenido a tu agenda de Amigos \n");
+	move(20,35);
+
+	attron(COLOR_PAIR(3));
+	init_pair(3,COLOR_BLUE,COLOR_BLACK);
+	attron(COLOR_PAIR(3));
+	printw("Laboratorio 5 de programacion");
+	getch();
+	clear();
+	echo();	
+	initscr();
+	start_color();
+	init_pair(1,COLOR_YELLOW,COLOR_BLACK);
+	attron(COLOR_PAIR(1));
+	//leerimprimir(nombre);
+	printw(nombre);
+	
 
 
 
 
+
+	
+	getch();
+	endwin();
 	return 0;
 }
+
+/*void leerimprimir(char* Friend){
+	move(19,30);
+	printw("Nombre: ");
+	getstr();
+	printw("Numero: ");
+	getstr(numero);	
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
